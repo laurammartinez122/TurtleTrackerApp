9f538d36c8ba4fb99b9d78f7e3aa905e75ce311d
laurammartinez122 <112514005+laurammartinez122@users.noreply.github.com>
2022-09-28 15:18:57 -0400

    Switch to while loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,16 +16,14 @@ file_name = "data/raw/Sara.txt"
 file_object = open(file = file_name, mode = "r")
 
 #Read contents of file into a list
-line_list = file_object.readlines()
-
-#Close the file object
-file_object.close()
+lineString = file_object.readline()
 
 #Extract one data line into a variable 
-for lineString in line_list:
+while lineString:
     
     #Check to see if the lineString is a data line
     if lineString[0] in ("#", "u"):
+        lineString = file_object.readline()
         continue
     
     #Split lineString into a list of items
@@ -38,5 +36,11 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
-#Print information to user
-print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}.")
+    #Print information to user
+    print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}.")
+    
+    #Move to the next line in the file 
+    lineString = file_object.readline()
+
+#Close the file object
+file_object.close()
