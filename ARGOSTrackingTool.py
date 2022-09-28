f1bd62311b09faf226a0466aa08f7f0f03d48c6d
laurammartinez122 <112514005+laurammartinez122@users.noreply.github.com>
2022-09-28 16:18:17 -0400

    else statement?

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -58,8 +58,14 @@ for the_key, the_value in date_dict.items():
     #See if the date (the value) matches the user date
     if the_value == user_date:
         matching_keys.append(the_key)
-    
+
+# Report whether no keys were found
+if len(matching_keys) == 0:
+    else: 
+        print(f"Sara was not located on {user_date}")
+        
 #Reveal locations for each key in matching_keys
 for matching_key in matching_keys:
     obs_lat, obs_lon = location_dict[matching_key]
     print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {user_date}.")
+    
